0b17358ec9acf74d36a1e44574a9417259cbe3da
MoDDem <moddem25@gmail.com>
2019-12-29 20:39:55 +0500

    Login view validation. User Name and Password binding (I have been read a lot about password binding, so i decide to use a simplest way. As someone said in stackoverflow comments: "If the bad guys have access to the RAM of your machine, you have bigger issues than them stealing your password.")

--- ViewModel/Pages/LoginViewModel.cs
+++ ViewModel/Pages/LoginViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,40 @@ using System.Windows;
 using MagicMine_Launcher.View.Pages;
 
 namespace MagicMine_Launcher.ViewModel.Pages {
-	class LoginViewModel : BaseVM {
+	class LoginViewModel : BaseVM, IDataErrorInfo {
+		private string username;
+		public string UserName {
+			get => username;
+			set => Set(ref username, value, nameof(UserName));
+		}
+
+		private string password;
+		public string Password {
+			get => password;
+			set => Set(ref password, value, nameof(Password));
+		}
+
+		public string Error => throw new NotImplementedException();
+
+		public string this[string columnName] {
+			get {
+				string error = string.Empty;
+				switch(columnName) {
+					case "UserName":
+						if(string.IsNullOrWhiteSpace(UserName)) {
+							error = "Username field must not be empty";
+							UserName = "";
+						}
+						break;
+					case "Password":
+						if(string.IsNullOrWhiteSpace(Password)) {
+							error = "Password field must not be empty";
+							Password = "";
+						}
+						break;
+				}
+				return error;
+			}
+		}
 	}
 }
